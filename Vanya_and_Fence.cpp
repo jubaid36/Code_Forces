3749cdc6c1ea8c485dd923ae6a2cbf934095dae8
MD Jubaid <jubaid369336@gmail.com>
2025-08-04 20:53:15 +0600

    Add New

--- Vanya_and_Fence.cpp
+++ Vanya_and_Fence.cpp
@@ -1,21 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>  // Includes all standard libraries
 using namespace std;
+
 int main()
 {
     int n, h;
-    cin >>n>>h;
-    int totalWith = 0;
-    for(int i = 0; i<n; i++)
+    cin >> n >> h;  // Read the number of friends (n) and the fence height (h)
+    
+    int totalWidth = 0;  // Initialize total required road width to zero
+
+    // Loop through each friend to read their height and calculate needed width
+    for (int i = 0; i < n; i++)
     {
-        int a;
-        cin>>a;
-        if(a>h)
+        int a;          // Variable to store the current friend's height
+        cin >> a;       // Input the height of the i-th friend
+
+        // If the friend's height is greater than the fence height,
+        // they need to bend down, which takes width = 2
+        if (a > h)
         {
-            totalWith += 2;
-        }else{
-            totalWith += 1;
+            totalWidth += 2;
+        }
+        else
+        {
+            // Otherwise, the friend walks normally, taking width = 1
+            totalWidth += 1;
         }
     }
-    cout<<totalWith;
-    return 0;
+
+    cout << totalWidth;  // Output the minimum total width of the road needed
+    return 0;            // End of program
 }
